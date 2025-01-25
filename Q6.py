5b1d805e9ed4af823393c6e351b7b04ac61fef7b
adwaiithaaa <adwaitha127@gmail.com>
2025-01-25 14:43:41 +0530

    Delete Q6.py

--- Q6.py
+++
@@ -1,8 +0,0 @@
-#6. PYTHON PROGRAM TO REVERSE OF A GIVEN NO.
-num=int(input("Enter the number to be reversed:"))
-newnum=0
-while(num>0):
-    rem=num%10
-    newnum=newnum*10+rem
-    num=num//10
-print("The reversed number is:",newnum)
