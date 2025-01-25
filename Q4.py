b30a85f88b4540551797275d9875c2dc446fd1c0
adwaiithaaa <adwaitha127@gmail.com>
2025-01-25 14:43:18 +0530

    Delete Q4.py

--- Q4.py
+++
@@ -1,8 +0,0 @@
-# 4. PYTHON PROGRAM TO SWAP TWO VARIABLES WITHOUT USING THIRD VARIABLE.
-numa=int(input("Enter 1st variable: "))
-numb=int(input("Enter 2nd variable: "))
-numa=numa+numb
-numb=numa-numb
-numa=numa-numb
-print("a:",numa)
-print("b:",numb)
